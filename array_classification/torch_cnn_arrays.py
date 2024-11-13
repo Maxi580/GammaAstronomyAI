bef95f917a49e71d8b57aa0e968fd50cdba18ec2
DemBackfisch <johannes.siegling@web.de>
2024-11-13 01:21:03 +0100

    removed unused imports

--- array_classification/torch_cnn_arrays.py
+++ array_classification/torch_cnn_arrays.py
@@ -2,9 +2,6 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import Dataset, DataLoader
-from torchvision import transforms
-from PIL import Image
-import numpy as np
 import os
 import json
 
