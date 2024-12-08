e0ab65d2e82b9f6f0e93d951853498f87b9d663d
Maxi580 <maxieichenseher@gmail.com>
2024-12-08 20:12:34 +0100

    Reduced Convolution for efficiency, to test results

--- arrayClassification/CNN/HexCNN.py
+++ arrayClassification/CNN/HexCNN.py
@@ -7,25 +7,20 @@ class HexCNN(nn.Module):
         super(HexCNN, self).__init__()
 
         self.features = nn.Sequential(
-            ConvHex(in_channels=1, out_channels=16, kernel_size=3),
+            ConvHex(in_channels=1, out_channels=16, kernel_size=2),
             nn.BatchNorm1d(16),
             nn.ReLU(),
             nn.Dropout1d(0.3),
 
-            ConvHex(in_channels=16, out_channels=32, kernel_size=3),
+            ConvHex(in_channels=16, out_channels=32, kernel_size=2),
             nn.BatchNorm1d(32),
             nn.ReLU(),
             nn.Dropout1d(0.4),
-
-            ConvHex(in_channels=32, out_channels=64, kernel_size=3),
-            nn.BatchNorm1d(64),
-            nn.ReLU(),
-            nn.Dropout1d(0.5),
         )
 
         self.classifier = nn.Sequential(
             nn.Flatten(),
-            nn.Linear(66496, 1024),  # 64*1039
+            nn.Linear(33248, 1024),  # 64*1039
             nn.ReLU(),
             nn.Dropout(0.3),
             nn.Linear(1024, 256),
