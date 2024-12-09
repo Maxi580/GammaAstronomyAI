80273f4ba8791802f64d3e16143abf6dd374a66c
Maxi580 <maxieichenseher@gmail.com>
2024-12-09 13:48:01 +0100

    Reduced RAM usage

--- arrayClassification/CNN/HexCNN.py
+++ arrayClassification/CNN/HexCNN.py
@@ -7,28 +7,28 @@ class HexCNN(nn.Module):
         super(HexCNN, self).__init__()
 
         self.features = nn.Sequential(
-            ConvHex(in_channels=1, out_channels=16, kernel_size=4),
-            nn.BatchNorm1d(16),
+            ConvHex(in_channels=1, out_channels=8, kernel_size=4),
+            nn.BatchNorm1d(8),
             nn.ReLU(),
             nn.Dropout1d(0.6),
 
-            ConvHex(in_channels=16, out_channels=32, kernel_size=3),
-            nn.BatchNorm1d(32),
+            ConvHex(in_channels=8, out_channels=16, kernel_size=3),
+            nn.BatchNorm1d(24),
             nn.ReLU(),
             nn.Dropout1d(0.5),
 
-            ConvHex(in_channels=32, out_channels=64, kernel_size=2),
-            nn.BatchNorm1d(64),
+            ConvHex(in_channels=16, out_channels=24, kernel_size=2),
+            nn.BatchNorm1d(24),
             nn.ReLU(),
             nn.Dropout1d(0.4),
         )
 
         self.classifier = nn.Sequential(
             nn.Flatten(),
-            nn.Linear(66496, 8192),
+            nn.Linear(24*1039, 6*1039),
             nn.ReLU(),
             nn.Dropout(0.3),
-            nn.Linear(8192, 512),
+            nn.Linear(6*1039, 512),
             nn.ReLU(),
             nn.Dropout(0.3),
             nn.Linear(512, 256),
