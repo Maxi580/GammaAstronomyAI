8aec4b5c09bab1406725f9e476d4bf4c5fc98ea6
Maxi580 <maxieichenseher@gmail.com>
2024-12-31 22:49:12 +0100

    Fixed Hyperparameter Tuning

--- hyperparameter_optimize.py
+++ hyperparameter_optimize.py
@@ -16,9 +16,9 @@ def create_model_with_params(trial):
         dropout_conv1=trial.suggest_float('dropout_conv1', 0.025, 0.5),
         dropout_conv2=trial.suggest_float('dropout_conv2', 0.025, 0.5),
         dropout_conv3=trial.suggest_float('dropout_conv3', 0.025, 0.5),
-        linear1_size=trial.suggest_int('linear1_size', 512, 38400, step=256, log=True),
-        linear2_size=trial.suggest_int('linear2_size', 128, 5096, step=64, log=True),
-        linear3_size=trial.suggest_int('linear3_size', 32, 2048, step=32, log=True),
+        linear1_size=trial.suggest_int('linear1_size', 512, 38400, step=1, log=True),
+        linear2_size=trial.suggest_int('linear2_size', 128, 5096, step=1, log=True),
+        linear3_size=trial.suggest_int('linear3_size', 32, 2048, step=1, log=True),
         dropout_linear1=trial.suggest_float('dropout_linear1', 0.025, 0.5),
         dropout_linear2=trial.suggest_float('dropout_linear2', 0.025, 0.5),
         dropout_linear3=trial.suggest_float('dropout_linear3', 0.025, 0.3)
@@ -28,7 +28,7 @@ def create_model_with_params(trial):
 def objective(trial, dataset: str, epochs: int):
     nametag = f"hexcnn_{dataset}_{time.strftime('%Y-%m-%d_%H-%M-%S')}_trial_{trial.number}"
     dataset_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "datasets", dataset)
-    output_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "/parameter_tuning/trained_models", nametag)
+    output_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "parameter_tuning/trained_models", nametag)
     os.makedirs(output_dir, exist_ok=True)
 
     supervisor = TrainingSupervisor("hexcnn", dataset_dir, output_dir)
@@ -49,10 +49,29 @@ def objective(trial, dataset: str, epochs: int):
     return supervisor.validation_metrics[-1]['accuracy']
 
 
-def main(dataset: str, epochs: int, n_trials: int):
-    study = optuna.create_study(direction='maximize', pruner=optuna.pruners.MedianPruner())
+def start_or_resume_study(dataset: str, epochs: int, n_trials: int):
+    try:
+        study = optuna.load_study(
+            study_name="hexcnn_optimization",
+            storage="sqlite:///optuna_study.db"
+        )
+        print("Resuming existing study")
+
+    except KeyError:
+        study = optuna.create_study(
+            study_name="hexcnn_optimization",
+            storage="sqlite:///optuna_study.db",
+            direction="maximize"
+        )
+        print("Creating new study")
+
     study.optimize(lambda trial: objective(trial, dataset, epochs), n_trials=n_trials)
 
+    return study
+
+
+def main(dataset: str, epochs: int, n_trials: int):
+    study = start_or_resume_study(dataset, epochs, n_trials)
     print("Best trial:")
     print(f" Value (Val Accuracy): {study.best_trial.value}")
     print(" Params:")
