3f0069d69124f63fb89419cbc5077c954b75e6b8
MaxiEichenseher <maxieichenseher@gmail.com>
2025-02-05 17:14:20 +0100

    its horrible

--- analyze_convolution.py
+++ analyze_convolution.py
@@ -3,53 +3,13 @@ import os
 import numpy as np
 import torch
 import torch.nn as nn
-import matplotlib.pyplot as plt
-from importlib.resources import files
-from ctapipe.instrument import CameraGeometry
-from ctapipe.visualization import CameraDisplay
 
 from CombinedNet.CombinedNet import CombinedNet
 from CNN.HexLayers.ConvHex import ConvHex
-from CNN.HexLayers.neighbor import unpool_array
 from CombinedNet.magicDataset import MagicDataset
+from magic_helper_functions import reconstruct_image
 
 POOLING_KERNEL_SIZE = 2
-
-
-def reconstruct_image(array_1039, save_path, title=None, pooling_count=0):
-    os.makedirs(os.path.dirname(save_path), exist_ok=True)
-
-    f = str(files("ctapipe_io_magic").joinpath("resources/MAGICCam.camgeom.fits.gz"))
-    geom = CameraGeometry.from_table(f)
-
-    if torch.is_tensor(array_1039):
-        array_1039 = array_1039.cpu().numpy()
-
-    fig, ax = plt.subplots(figsize=(10, 10))
-    disp = CameraDisplay(geom, ax=ax)
-
-    if pooling_count > 0:
-        array_1039, valid_indices = unpool_array(
-            array_1039,
-            pooling_kernel_size=2,
-            num_pooling_layers=POOLING_KERNEL_SIZE
-        )
-
-        mask = np.ones_like(array_1039, dtype=bool)
-        mask[valid_indices] = False
-        masked_data = np.ma.masked_array(array_1039, mask=mask)
-        disp.image = masked_data
-        disp.cmap.set_bad('gray', alpha=0.2)
-    else:
-        disp.image = array_1039
-
-    if title:
-        plt.title(title)
-
-    plt.colorbar(disp.pixels, ax=ax, label='Intensity')
-    plt.tight_layout()
-    plt.savefig(save_path)
-    plt.close()
 
 
 def extract_conv_arrays(tensor, cnn_idx, pooling_count, output_dir):
