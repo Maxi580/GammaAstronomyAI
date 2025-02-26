65a4bc1f921ab285321dce76495171881220289e
DemBackfisch <johannes.siegling@web.de>
2025-02-26 16:28:24 +0100

    read optuna for multi-value optimization

--- ReadOptuna.py
+++ ReadOptuna.py
@@ -7,31 +7,42 @@ def read_study_results(study_name: str, storage_url: str):
             study_name=study_name,
             storage=storage_url
         )
-
-        print("Best trial:")
-        print(f"  Trial number: {study.best_trial.number}")
-        print(f"  Value (Validation Accuracy): {study.best_trial.value}")
-        print("  Parameters:")
-        for key, value in study.best_trial.params.items():
-            print(f"    {key}: {value}")
-        print("\n" + "=" * 50 + "\n")
-
-        print(f"All trials for study '{study_name}':")
-        for trial in study.trials:
-            print(f"  Trial number: {trial.number}")
-            print(f"    Value: {trial.value}")
-            print(f"    Parameters:")
+        
+        # print("Best trials:")
+        # for trial in study.best_trials:
+        #     print(f"Trial #{trial.number}")
+        #     print(f"  Values: {trial.values}")
+        #     print("  Params:")
+        #     for key, value in trial.params.items():
+        #         print(f"    {key}: {value}")
+                
+        filtered = list(filter(lambda x: x.values is not None and x.values[0] > 84, study.best_trials))
+        filtered.sort(key=lambda x: x.values[1], reverse=True)
+        
+        print("Best trials above 84%:")
+        for trial in filtered:
+            print(f"Trial #{trial.number}")
+            print(f"  Values: {trial.values}")
+            print("  Params:")
             for key, value in trial.params.items():
-                print(f"      {key}: {value}")
-            print(f"    State: {trial.state}")
-            print("-" * 50)
+                print(f"    {key}: {value}")
+
+        # print(f"All trials for study '{study_name}':")
+        # for trial in study.trials:
+        #     print(f"  Trial number: {trial.number}")
+        #     print(f"    Value: {trial.values}")
+        #     print(f"    Parameters:")
+        #     for key, value in trial.params.items():
+        #         print(f"      {key}: {value}")
+        #     print(f"    State: {trial.state}")
+        #     print("-" * 50)
 
     except Exception as e:
         print(f"Failed to read the study: {e}")
 
 
 if __name__ == "__main__":
-    study_name = "OptimizeHexCNN"
+    study_name = "Optimize_hexcirclenet_New"
     storage_url = "sqlite:///optuna_study.db"
 
     read_study_results(study_name, storage_url)
